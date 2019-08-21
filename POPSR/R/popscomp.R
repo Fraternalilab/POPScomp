2484ef482b58aa0682cedbdefc74c42430d81d62
jkleinj <jens@jkleinj.eu>
2019-08-21 22:33:30 +0100

    per-chain POPScomp works, but outdir does not for rout

--- POPSR/R/popscomp.R
+++ POPSR/R/popscomp.R
@@ -12,142 +12,44 @@ library("bio3d");
 
 library("parallel");
 
-popsR = function() {
+popscompR = function(inputPDB, outdir) {
 	## number of cores
 	nCore = detectCores() - 1;
 
-	## all PDB structures
-	inFileDir = ".";
-	inFileName = list.files(path = inFileDir, full.names = FALSE, pattern = 'centroid.pdb$');
+	#________________________________________________________________________________
+	## split input PDB into chains
+	inputPDB = "1f3r.pdb";
+	outdir = tempdir();
+	chain.files = pdbsplit(inputPDB,  path = outdir, multi = FALSE);
+	## short names
+	chain.files.short = sub('\\.pdb$', '', basename(chain.files));
 
-	## general output directory
-	outDir = "popscomp";
-	dir.create(file.path(".", outDir), showWarnings = FALSE);
+	#________________________________________________________________________________
+	## create temporary PDB files for all pairwise chain combinations
+	pair.cmbn = combn(length(chain.files), 2);
+	chainpair.files = list();
+	chainpair.files = sapply(1:dim(pair.cmbn)[2], function(x) {
+	  chainpair.files[[x]] = paste0(outdir, "/",
+	                          chain.files.short[pair.cmbn[1, x]], "-",
+	                          chain.files.short[pair.cmbn[2, x]], ".pdb");
+	  command = paste("cat", chain.files[pair.cmbn[1, x]],
+	                         chain.files[pair.cmbn[2, x]], ">",
+	                         chainpair.files[[x]]);
+	  system_status = system(command);
+	  paste("Chain pair:", x, "; Exit code:", system_status);
+	  return(chainpair.files);
+	});
 
-	#_______________________________________________________________________________
-	## process domains 
-	for (i in 1:length(inFileName)) {
-		pdb = read.pdb(paste(inFileDir, inFileName[i], sep = "/"));
-
-		## structure-specific output directory
-		outStrDir = substr(inFileName[i], 1, nchar(inFileName[i]) - 4);
-		dir.create(file.path(outDir, outStrDir), showWarnings = FALSE);
-
-		#_______________________________________________________________________________
-		## rename chains of domains
-		## rename chain of N-terminal domain (first residue set to 1, min is 1)
-		pdb$atom$chain[pdb$atom$resno %in% c(1:47)] = "N";
-		## create PDB of N-terminal domain
-		chainN = atom.select(pdb, resno = c(1:47));
-		pdbChainN = trim.pdb(pdb, chainN);
-
-		## rename chain of A domain
-		pdb$atom$chain[pdb$atom$resno %in% c(48:116, 221:388)] = "A";
-		## create PDB of A domain
-		chainA = atom.select(pdb, resno = c(48:116, 221:388));
-		pdbChainA = trim.pdb(pdb, chainA);
-
-		## rename chain of B domain
-		pdb$atom$chain[pdb$atom$resno %in% c(117:220)] = "B";
-		## create PDB of B domain
-		chainB = atom.select(pdb, resno = c(117:220));
-		pdbChainB = trim.pdb(pdb, chainB);
-
-		## rename chain of C domain (last residue set to 550, max is 533)
-		pdb$atom$chain[pdb$atom$resno %in% c(389:550)] = "C";
-		## create PDB of C domain
-		chainC = atom.select(pdb, resno = c(389:550));
-		pdbChainC = trim.pdb(pdb, chainC);
-
-		#_______________________________________________________________________________
-		## write out complete PDB structure: all domains with renamed chain(s)
-		outFileNameComplete = paste("pdb", ".complete", ".pdb", sep = "");
-		write.pdb(pdb, file = paste(outDir, outStrDir, outFileNameComplete, sep = "/"));
-
-		#_______________________________________________________________________________
-		## write out single domains
-		## N-terminal domain
-		outFileNameDom = list();
-		doms = list(pdbChainN, pdbChainA, pdbChainB, pdbChainC);
-		domsName = c("Ndom", "Adom", "Bdom", "Cdom");
-
-		outFileNameDom[[1]] = paste(domsName[1], "pdb", sep = ".");
-		write.pdb(doms[[1]], file = paste(outDir, outStrDir, outFileNameDom[1], sep = "/"));
-
-		## A domain
-		outFileNameDom[[2]] = paste(domsName[2], "pdb", sep = ".");
-		write.pdb(doms[[2]], file = paste(outDir, outStrDir, outFileNameDom[[2]], sep = "/"));
-
-		## B domain
-		outFileNameDom[[3]] = paste(domsName[3], "pdb", sep = ".");
-		write.pdb(doms[[3]], file = paste(outDir, outStrDir, outFileNameDom[[3]], sep = "/"));
-
-		## C domain
-		outFileNameDom[[4]] = paste(domsName[4], "pdb", sep = ".");
-		write.pdb(doms[[4]], file = paste(outDir, outStrDir, outFileNameDom[[4]], sep = "/"));
-
-		#_______________________________________________________________________________
-		## write out all domain pairs
-		pair.cbn = combn(1:4, 2, simplify = FALSE);
-		outFileNameDomPair = list();
-
-		for (j in 1:length(pair.cbn)) {
-			## concatenate domains
-			domPair = cat.pdb(doms[[pair.cbn[[j]][1]]], doms[[pair.cbn[[j]][2]]],
-								renumber = TRUE, rechain = FALSE);
-			outFileNameDomPair[[j]] = paste(
-							domsName[pair.cbn[[j]][1]],
-							domsName[pair.cbn[[j]][2]],
-							"pdb", sep = ".");
-			write.pdb(domPair, file = paste(outDir, outStrDir, outFileNameDomPair[[j]], sep = "/"));
-		}
-
-		#_______________________________________________________________________________
-		## POPS all of the above structures (complete, single domain, paired domains)
-		## all filenames
-		outFileNameAll = list(outFileNameComplete, outFileNameDom, outFileNameDomPair);
-		## simplify list structure
-		outFileNameAll.l = as.list(unlist(outFileNameAll));
-		outFileNameDirAll.l = as.list(paste(outDir, outStrDir, unlist(outFileNameAll), sep = "/"));
-
-		#_______________________________________________________________________________
-		## parallelisation
-		## initiate cluster for parallel computation 
-		#clu = makeCluster(nCore);
-		## make parallel functions see predefined variables
-		#clusterExport(clu, c("outFileNameDirAll.l"));
-
-		#_______________________________________________________________________________
-		## POPS all derived structures of one input structure
-		#parLapply(clu, outFileNameDirAll.l, function(x) {
-		#	print(paste("./pops --pdb ", x, " --popsOut ", x, ".out", " --popsbOut ", x, ".bout", " --sigmaOut ", x, ".sout", sep = "")); });
-		lapply(outFileNameDirAll.l, function(x) {
-			system(paste("./pops --pdb ", x, " --popsOut ", x, ".out", " --popsbOut ", x, ".bout", " --sigmaOut ", x, ".sout", " --coarse --rout", sep = "")); });
-			#print(paste("./pops --pdb ", x, " --popsOut ", x, ".out", " --popsbOut ", x, ".bout", " --sigmaOut ", x, ".sout", " --coarse --rout", sep = "")); });
-
-		## release memory of parallelised structure
-		#stopCluster(clu);
-
-		#_______________________________________________________________________________
-		## POPSCOMP : compute $DeltaSASA in complex [(X + Y) - XY]
-		## for all domain pairs
-		outFileNameDeltaSasa = list();
-		for (j in 1:length(pair.cbn)) {
-			domXY.molSasa.outFileName = paste(outFileNameDomPair[[j]], "out.rpopsMolecule", sep = ".");
-			domXY.molSasa = read.table(paste(outDir, outStrDir, domXY.molSasa.outFileName, sep = "/"), header = TRUE);
-			domX.molSasa.outFileName = paste(outFileNameDom[[pair.cbn[[j]][1]]], "out.rpopsMolecule", sep = ".");
-			domX.molSasa = read.table(paste(outDir, outStrDir, domX.molSasa.outFileName, sep = "/"), header = TRUE);
-			domY.molSasa.outFileName = paste(outFileNameDom[[pair.cbn[[j]][2]]], "out.rpopsMolecule", sep = ".");
-			domY.molSasa = read.table(paste(outDir, outStrDir, domY.molSasa.outFileName, sep = "/"), header = TRUE);
-			## deltaSASA
-			delta.molSasa = (domX.molSasa + domY.molSasa) - domXY.molSasa; 
-			outFileNameDeltaSasa[[j]] = paste(
-							domsName[pair.cbn[[j]][1]],
-							domsName[pair.cbn[[j]][2]],
-							"dsasaMolecule", sep = ".");
-			write.table(delta.molSasa, file = paste(outDir, outStrDir, outFileNameDeltaSasa[[j]], sep = "/"));
-		}
-	}
+	#________________________________________________________________________________
+	## run POPS over all pairwise chain combinations via system (= shell) call
+	sapply(1:length(chainpair.files), function(x) {
+	  command = paste0("pops --outDirName ", outdir,
+	                  " --rout --atomOut --residueOut --chainOut ",
+	                  "--pdb ", chainpair.files[x], " 1> POPScomp_chainpair", x, ".o",
+                                                 " 2> POPScomp_chainpair", x, ".e");
+	  system_status = system(command);
+	  paste("Exit code:", system_status);
+	});
 }
 
 #===============================================================================
