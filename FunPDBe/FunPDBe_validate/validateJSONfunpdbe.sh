c7b72e34e53740b93ee6b854bb485b8744df7aaa
jkleinj <jens@jkleinj.eu>
2020-01-14 17:12:06 +0000

    correction in Makefile for 'make dist'

--- FunPDBe/FunPDBe_validate/validateJSONfunpdbe.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#===============================================================================
-# Validate JSON output against funpdbe server
-# see also ~/POPS/funpdbe-client/README.md
-#===============================================================================
-
-../funpdbe-client/funpdbe_client.py --path=$1 --mode=validate
-
