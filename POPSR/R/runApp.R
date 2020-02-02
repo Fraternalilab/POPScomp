f8e9632be8fabbd11d56341fd7ce8acb71077d51
jkleinj <jens@jkleinj.eu>
2020-02-02 10:31:01 +0000

    added POPScomp logo

--- POPSR/R/runApp.R
+++ POPSR/R/runApp.R
@@ -7,7 +7,6 @@
 #===============================================================================
 
 ## https://stackoverflow.com/questions/37830819/developing-shiny-app-as-a-package-and-deploying-it-to-shiny-server#37833866
-
 ## To make your new package’s shiny app runnable from the shiny server
 ##   create a file /srv/shiny-server/myapp/app.R with the following code
 ##   and your app will be available at http://<your_shiny_server_url>/myapp :
