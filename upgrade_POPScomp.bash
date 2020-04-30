f594fcffde523112ead10f384474fc393a260d7b
Jens (The Maker) <jens@jkleinj.eu>
2020-04-30 19:22:38 +0100

    added upgrade script

---
+++ upgrade_POPScomp.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+## remove old pops binary
+rm POPSC/src/pops
+
+## pull new code from GitHub
+git pull https://github.com/Fraternalilab/POPScomp
+
+## compile POPSC
+cd POPSC ; ./configure ; make ; cd ..
+
+## compile POPSR
+R CMD build POPSR
+ls -1 POPSR_*.tar.gz | tail -1 | xargs -i bash -c "R CMD INSTALL {}"
+
