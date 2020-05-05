299881357df18ed40b51386b60066b56cd799249
Jens (The Maker) <jens@jkleinj.eu>
2020-05-05 17:54:40 +0100

    removed old directory

--- FunPDBe/old.FunPDBe_uploadViaClient/uploadJSON.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#===============================================================================
-# Upload JSON output to FunPDBe server
-# see also ~/POPS/funpdbe-client/README.md
-#===============================================================================
-
-../funpdbe-client/funpdbe_client.py --user="test-popscomp" --pwd="funpdbe-test" --api=dev --mode=post --path=$1 --resource popscomp --overwrite 
-
