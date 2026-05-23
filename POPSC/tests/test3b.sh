c299ca083e5dfa044a1663b9d0799100ab0b7d04
Jens (The Maker) <jens@jkleinj.eu>
2026-05-23 22:51:35 +0100

    correction in tests

--- POPSC/tests/test3b.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "--------------------------------------------------------------"
-echo " test3b.sh                                                    "
-echo "--------------------------------------------------------------"
-
-mpirun -n 2 -hostfile host_file ../src/pops --pdb 1aki.pdb --traj 1aki.sdtraj.gro --compositionOut --typeOut --topologyOut --atomOut --residueOut --chainOut || exit 1
-
