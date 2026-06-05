8d623602c05e0c4d64f07d18b46e6bc03ac96380
Jens (The Maker) <jens@jkleinj.eu>
2026-06-05 12:54:16 +0100

    corected version number

--- POPSR/inst/popsr/app.R
+++ POPSR/inst/popsr/app.R
@@ -239,7 +239,7 @@ ui <- fluidPage(
         ),
         tabPanel("About",
 			h3("Shiny App"),
-			p("This is version 3.8 of the POPScomp Shiny App."),
+			p("This is version 3.4 of the POPScomp Shiny App."),
 			p("For detailed information about the software visit Fraternali Lab's ",
 			  a("POPScomp GitHub repository", href="https://github.com/Fraternalilab/POPScomp"),
 			  "; the Wiki pages contain detailed installation and usage instructions."
